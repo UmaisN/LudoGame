2b044c0f1e0b6b4b21caa9ec3b836deeafbcfff4
Salar Abbas <salarabbas2000@gmail.com>
2021-06-14 18:59:21 +0500

    Alot of stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,14 @@
 #include <iostream>
 #include <time.h>
 #include <vector>
+#include <cstdlib>
 using namespace sf;
 
 
 
-
-int main()
-{
-   RenderWindow window(VideoMode(720,720), "Ludo Game");
-    window.setFramerateLimit(60);
-
-    //YELLOW TOKENS LOCATIONS
+//---------_________YELLOW TEAM CLASS_______--------//
+class YellowTeam {
+  public:
  float Y1x=25;
  float Y1y=570;
  float Y2x=25;
@@ -21,8 +18,34 @@ int main()
  float Y3y=630;
  float Y4x=90;
  float Y4y=570;
- //RED TOKENS LOCATIONS
-
+ Texture TokY1;
+ Texture TokY2;
+ Texture TokY3;
+ Texture TokY4;
+ Sprite YellowToken1;
+ Sprite YellowToken2;
+ Sprite YellowToken3;
+ Sprite YellowToken4;
+   public:
+ YellowTeam()
+   {
+   TokY2.loadFromFile("images/Yellow.png");
+   TokY3.loadFromFile("images/Yellow.png");
+   TokY4.loadFromFile("images/Yellow.png");
+   TokY1.loadFromFile("images/Yellow.png");
+   YellowToken1.setTexture(TokY1);
+   YellowToken2.setTexture(TokY2);
+   YellowToken3.setTexture(TokY3);
+   YellowToken4.setTexture(TokY4);
+   YellowToken1.setPosition(Y2x, Y2y);
+   YellowToken2.setPosition(Y1x, Y1y);
+   YellowToken3.setPosition(Y3x, Y3y);
+   YellowToken4.setPosition(Y4x, Y4y);
+   }
+};
+//__________-------RED TEAM CLASS---------__________//
+class RedTeam {
+ public:
  float R1x=565;
  float R1y=25;
  float R2x=565;
@@ -31,7 +54,36 @@ int main()
  float R3x=630;
  float R4y=85;
  float R4x=630;
-
+ Texture TokR1;
+ Texture TokR2;
+ Texture TokR3;
+ Texture TokR4;
+ Sprite RedToken1;
+ Sprite RedToken2;
+ Sprite RedToken3;
+ Sprite RedToken4;
+   public:
+  RedTeam()
+   {
+   TokR2.loadFromFile("images/Red.png");
+   TokR3.loadFromFile("images/Red.png");
+   TokR4.loadFromFile("images/Red.png");
+   TokR1.loadFromFile("images/Red.png");
+   RedToken1.setTexture(TokR1);
+   RedToken2.setTexture(TokR2);
+   RedToken3.setTexture(TokR3);
+   RedToken4.setTexture(TokR4);
+     RedToken1.setPosition(R2x, R2y);
+     RedToken2.setPosition(R1x, R1y);
+      RedToken3.setPosition(R3x, R3y);
+       RedToken4.setPosition(R4x, R4y);
+   }
+};
+//--------________RED TEAM CLASS__________------------//
+//-------_________BLUE TEAM CLASS__________-----------//
+class BlueTeam
+{
+public:
  //BLUE TOKEN LOCATIONS
  float B1x=25;
  float B1y=25;
@@ -41,7 +93,34 @@ int main()
  float B3y=25;
  float B4x=90;
  float B4y=85;
- //GREEN TOKEN LOCATIONS;
+ Texture TokB1;
+ Texture TokB2;
+ Texture TokB3;
+ Texture TokB4;
+ Sprite BlueToken1;
+ Sprite BlueToken2;
+ Sprite BlueToken3;
+ Sprite BlueToken4;
+ public:
+ BlueTeam()
+ {
+    TokB2.loadFromFile("images/Blue.png");
+   TokB3.loadFromFile("images/Blue.png");
+   TokB4.loadFromFile("images/Blue.png");
+   TokB1.loadFromFile("images/Blue.png");
+   BlueToken1.setTexture(TokB1);
+   BlueToken2.setTexture(TokB2);
+   BlueToken3.setTexture(TokB3);
+   BlueToken4.setTexture(TokB4);
+   BlueToken1.setPosition(B1x, B1y);
+BlueToken2.setPosition(B2x, B2y);
+BlueToken3.setPosition(B3x, B3y);
+BlueToken4.setPosition(B4x, B4y);
+ }
+};
+//-----______GREEN TEAM CLASS------____________//
+class GreenTeam {
+  public:
  float G1x=565;
  float G1y=570;
  float G2x=565;
@@ -50,65 +129,86 @@ int main()
  float G3y=630;
  float G4x=630;
  float G4y=570;
-//ARRAY STORING THESE LOCATIONS
-float StartingLocations[4][4][2];
-  //THE 4 TOKENS FOR YELLOW TEAM
-  
-  Texture TokY1;
-  Texture TokY2;
-  Texture TokY3;
-  Texture TokY4;
-   TokY2.loadFromFile("images/Yellow.png");
-   TokY3.loadFromFile("images/Yellow.png");
-   TokY4.loadFromFile("images/Yellow.png");
-   TokY1.loadFromFile("images/Yellow.png");
-   Sprite YellowToken1(TokY1);
-   Sprite YellowToken2(TokY2);
-   Sprite YellowToken3(TokY3);
-   Sprite YellowToken4(TokY4);
-   //------------------------//
-   //THE 4 TOKENS FOR BLUE TEAM
-  Texture TokR1;
-  Texture TokR2;
-  Texture TokR3;
-  Texture TokR4;
-   TokR2.loadFromFile("images/Blue.png");
-   TokR3.loadFromFile("images/Blue.png");
-   TokR4.loadFromFile("images/Blue.png");
-   TokR1.loadFromFile("images/Blue.png");
-   Sprite BlueToken1(TokR1);
-   Sprite BlueToken2(TokR2);
-   Sprite BlueToken3(TokR3);
-   Sprite BlueToken4(TokR4);
-   //--------------------------//
-    //THE 4 TOKENS FOR REDTEAM
-  Texture TokB1;
-  Texture TokB2;
-  Texture TokB3;
-  Texture TokB4;
-   TokB2.loadFromFile("images/Red.png");
-   TokB3.loadFromFile("images/Red.png");
-   TokB4.loadFromFile("images/Red.png");
-   TokB1.loadFromFile("images/Red.png");
-   Sprite RedToken1(TokB1);
-   Sprite RedToken2(TokB2);
-   Sprite RedToken3(TokB3);
-   Sprite RedToken4(TokB4);
-   //--------------------------//
-    //THE 4 TOKENS FOR GREEN TEAM
-  Texture TokG1;
+ Texture TokG1;
   Texture TokG2;
   Texture TokG3;
   Texture TokG4;
+   Sprite GreenToken1;
+   Sprite GreenToken2;
+   Sprite GreenToken3;
+   Sprite GreenToken4;
+   public:
+  GreenTeam()
+   {
    TokG2.loadFromFile("images/Green.png");
    TokG3.loadFromFile("images/Green.png");
    TokG4.loadFromFile("images/Green.png");
    TokG1.loadFromFile("images/Green.png");
-   Sprite GreenToken1(TokG1);
-   Sprite GreenToken2(TokG2);
-   Sprite GreenToken3(TokG3);
-   Sprite GreenToken4(TokG4);
-   //--------------------------//
+   GreenToken1.setTexture(TokG1);
+   GreenToken2.setTexture(TokG2);
+   GreenToken3.setTexture(TokG3);
+   GreenToken4.setTexture(TokG4);
+   GreenToken1.setPosition(G1x, G1y);
+   GreenToken4.setPosition(G4x, G4y);
+   GreenToken3.setPosition(G3x, G3y);
+   GreenToken2.setPosition(G2x, G2y);
+   }
+
+};
+//______________THE DICE CLASS________________//
+class Dice
+{
+public:
+Texture D1,D2,D3,D4,D5,D6,D7;
+float RDAx = 750;
+float RDAy =  150;
+Sprite DS[6];
+public:
+Dice()
+{
+D1.loadFromFile("images/Dice1.png");
+D2.loadFromFile("images/Dice2.png");
+D3.loadFromFile("images/Dice3.png");
+D4.loadFromFile("images/Dice4.png");
+D5.loadFromFile("images/Dice5.png");
+D6.loadFromFile("images/Dice6.png");
+DS[0].setTexture(D1);
+DS[1].setTexture(D2);
+DS[2].setTexture(D3);
+DS[3].setTexture(D4);
+DS[4].setTexture(D5);
+DS[5].setTexture(D6);
+DS[0].setPosition(RDAx,RDAy);
+DS[1].setPosition(RDAx,RDAy);
+DS[2].setPosition(RDAx,RDAy);
+DS[3].setPosition(RDAx,RDAy);
+DS[4].setPosition(RDAx,RDAy);
+DS[5].setPosition(RDAx,RDAy);
+}
+int RollTheDice()
+{
+int RandomNumber = rand();
+RandomNumber %=6;
+RandomNumber +=18;
+return RandomNumber;
+}
+
+};
+//_______________________THE_______________________//
+//______________________MAIN________________________//
+//_____________________FUNCTION_____________________//
+int main()
+{
+  sf::Time T1 = sf::seconds(0.2f);
+  RenderWindow window(VideoMode(900,900), "Ludo Game");
+  window.setFramerateLimit(60);
+  //DECLARING CLASS VARIABLES//
+GreenTeam G;
+BlueTeam B;
+RedTeam R;
+YellowTeam Y;
+Dice D;
+//_______________//
     Texture bg;
     bg.loadFromFile("images/board.png",sf::IntRect(15,15,720,720));
     Sprite background(bg);
@@ -120,57 +220,59 @@ float StartingLocations[4][4][2];
           if (e.type == Event::Closed)
             window.close();
  }
+ //DRAWING THE TOKENS__________//
  window.draw(background);
- //DRAWING YELLOW TEAMS TOKENS
- YellowToken1.setPosition(Y1x, Y1y);
- window.draw(YellowToken1);
-  YellowToken2.setPosition(Y2x, Y2y);
- window.draw(YellowToken2);
-  YellowToken3.setPosition(Y3x, Y3y);
- window.draw(YellowToken3);
-  YellowToken4.setPosition(Y4x, Y4y);
- window.draw(YellowToken4);
+ window.draw(Y.YellowToken1);
+ window.draw(Y.YellowToken2);
+ window.draw(Y.YellowToken3);
+ window.draw(Y.YellowToken4);
  //----------------------------------//
  //DRAWING YELLOW TEAMS TOKENS
- BlueToken1.setPosition(B1x, B1y);
- window.draw(BlueToken1);
-  BlueToken2.setPosition(B2x, B2y);
- window.draw(BlueToken2);
-  BlueToken3.setPosition(B3x, B3y);
- window.draw(BlueToken3);
-  BlueToken4.setPosition(B4x, B4y);
- window.draw(BlueToken4);
+
+ window.draw(B.BlueToken1);
+ window.draw(B.BlueToken2);
+ window.draw(B.BlueToken3);
+ window.draw(B.BlueToken4);
  //----------------------------------//
   //DRAWING RED TEAMS TOKENS
- RedToken1.setPosition(R1x, R1y);
- window.draw(RedToken1);
-  RedToken2.setPosition(R2x, R2y);
- window.draw(RedToken2);
-  RedToken3.setPosition(R3x, R3y);
- window.draw(RedToken3);
-  RedToken4.setPosition(R4x, R4y);
- window.draw(RedToken4);
+ window.draw(R.RedToken1);
+ window.draw(R.RedToken2);
+ window.draw(R.RedToken3);
+ window.draw(R.RedToken4);
  //----------------------------------//
   //----------------------------------//
   //DRAWING GREEN TEAMS TOKENS
-// GreenToken1.setPosition(G1x, G1y);
- window.draw(GreenToken1);
-  GreenToken2.setPosition(G2x, G2y);
- window.draw(GreenToken2);
-  GreenToken3.setPosition(G3x, G3y);
- window.draw(GreenToken3);
-  GreenToken4.setPosition(G4x, G4y);
- window.draw(GreenToken4);
+ 
+ window.draw(G.GreenToken1);
+ window.draw(G.GreenToken2);
+ window.draw(G.GreenToken3);
+ window.draw(G.GreenToken4);
  //----------------------------------//
-      window.display();
+      
 
-      if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+      //FUNCTION TO SHOW DICE ANIMATION
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R))
       {
-        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-        GreenToken1.setPosition((float)mousePos.x,static_cast<float>(mousePos.y));
-        std::cout <<"JIFND";
-;
-      }
+        int RandomNumber = rand();
+        RandomNumber %=6;
+        RandomNumber+=18;
+        std::cout << RandomNumber <<std::endl;
+        for(int i=0;i<RandomNumber;i++)
+        {
+          int j=i;
+          if(i> 5)
+          {
+            j %=5;
+          }
+           window.draw(D.DS[j]);
+            window.display();
+       sleep(T1);
+          }
+      
+        }
+
+        
+     window.display();
       }
     return 0;
 }
